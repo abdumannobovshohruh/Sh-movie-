6831375282aa318d667abafba02a4f611bf07331
shohruh973 <abdumannobovshohruh973@gmail.com>
2022-12-21 17:56:50 +0500

    Sh-movie

---
+++ index.js
@@ -0,0 +1,12 @@
+const API_URL ='https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1'
+const IGM_PATH ='https://image.tmdb.org/t/p/w1280'
+
+
+
+let a =  fetch ('API_URL')
+
+
+
+
+
+console.log(a);
